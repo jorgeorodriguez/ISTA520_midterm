48d936cedbf0337a888c02bf51242c05a871e084
jorgeorodriguez <jorgeorodriguez@email.arizona.edu>
2013-10-22 00:01:17 -0700

    Update grav_wq.py

--- grav_wq.py
+++ grav_wq.py
@@ -8,6 +8,8 @@
 from work_queue import *
 import sys
 
+start_timestep = 1
+end_timestep = 55
 
 
 num_args = 4 # including the script name
@@ -27,15 +29,15 @@ elif len(sys.argv) <> num_args:
 
 
 grav_calc = sys.argv[1]+"/scripts/"+sys.argv[0]
-density_file = sys.argv[2]+"/inputs/density_grid.txt"
+#density_file = sys.argv[2]+"/inputs/density_grid.txt"
 grav_pos_file = sys.argv[2]+"/inputs/grav_pos.txt"
 
 
 try:
-	Q = WorkQueue(port=0)
+        Q = WorkQueue(port=0)
 except:
-	print "could not instantiate Work Queue master"
-	sys.exit(1)
+        print "could not instantiate Work Queue master"
+        sys.exit(1)
 
 print "Listening on port %d." % Q.port
 
@@ -45,17 +47,18 @@ gp = open(grav_pos_file, 'r')
 
 count = 0
 for line in gp:
-	gp_list.append(line.split())
-	command = "python grav.py " + density_file + " %s %s %s %s" % (gp_list[count][0], gp_list[count][1], gp_list[count][2], gp_list[count][3])
-	#print command
-	outfile = str(gp_list[count][0]) +"_" + density_file +".out"
-	#print outfile
-	count += 1
-	T = Task(command)
-	T.specify_file(grav_file, grav_file, WORK_QUEUE_INPUT, cache = TRUE)
-	T.specify_file(density_file, density_file, WORK_QUEUE_INPUT, cache = TRUE)
-	T.specify_file(outfile, outfile, WORK_QUEUE_OUTPUT, cache = FALSE)
-	taskid = Q.submit(T)
+        gp_list.append(line.split())
+        for i in range(start_timestep, end_timestep):
+                command = "python grav.py" +i+"_density_grid.txt %s %s %s %s" % (gp_list[count][0], gp_list[count][1], gp_list[count][2], gp_list[count][3])
+                print command
+                outfile = sys.argv[3]+"/outputs/"+str(gp_list[count][0]) +"_"+i+"_density_grid.txt.out"
+                print outfile
+                count += 1
+                T = Task(command)
+                T.specify_file(grav_file, grav_file, WORK_QUEUE_INPUT, cache = TRUE)
+                T.specify_file(density_file, density_file, WORK_QUEUE_INPUT, cache = TRUE)
+                T.specify_file(outfile, outfile, WORK_QUEUE_OUTPUT, cache = FALSE)
+                taskid = Q.submit(T)
 print "Done."
 gp.close()
 sys.exit(0)
@@ -66,6 +69,6 @@ while not Q.empty():
     T = Q.wait(5)
     if T:
         print "Task (id# %d) complete: %s (return code %d)" % (T.id, T.command, T.return_status)
-    	print T.result
+            print T.result
 
 print "done."
