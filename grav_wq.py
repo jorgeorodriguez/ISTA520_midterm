5acf8318067ef69650a53f2d589a261955b38b4e
jorgeorodriguez <jorgeorodriguez@email.arizona.edu>
2013-10-27 16:15:28 -0700

    Update grav_wq.py

--- grav_wq.py
+++ grav_wq.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 # This script uses grav.py functions to compute gravity for
 # a single point whose data is provided on the command line. 
-# Input: See print_usage() for the input parameters and their order.
+# Input: See print_usage() for the input parameter.
 # Output: a single file that with one line containing the
 # position ID, timestep and gravity measurement.
 # Author: Rachel Gladysz 
@@ -9,35 +11,35 @@ from work_queue import *
 import sys
 
 start_timestep = 1
-end_timestep = 55
+#end_timestep = 55
+end_timestep = 1
 
+PORT_ID = 54601
 
-num_args = 4 # including the script name
+queue_wait_time = 10 # how many seconds to wait for the results
+
+num_args = 1 # including the script name
 
 def print_usage():
-    print "Usage: python ", sys.argv[0], " <Path to scripts> <Path to inputs> <Path to outputs>"
-    print "<Path to scripts> : Indicate which directory contains the scripts file"
-    print "<Path to inputs> : Indicate which directory contains the inputs file"
-    print "<Path to outputs> : Indicate which directory contains the outputs file"
+    print "Usage: python ", sys.argv[0] 
+    #print "sys.argv[0] : Indicate the script file name"
     sys.exit(1)
 
-if (len(sys.argv) == 1):
+if (len(sys.argv) != 1):
     print_usage()
 elif len(sys.argv) <> num_args:
     print "The script expects %d arguments. You provided %d." % (num_args, len(sys.argv))
     print_usage()
 
 
-grav_calc = sys.argv[1]+"/scripts/"+sys.argv[0]
-#density_file = sys.argv[2]+"/inputs/density_grid.txt"
-grav_pos_file = sys.argv[2]+"/inputs/grav_pos.txt"
-
+grav_pos_file = "grav_pos.txt"
+script_file = "grav_per_point.py"
 
 try:
-        Q = WorkQueue(port=0)
+	Q = WorkQueue(port=PORT_ID)
 except:
-        print "could not instantiate Work Queue master"
-        sys.exit(1)
+	print "could not instantiate Work Queue master"
+	sys.exit(1)
 
 print "Listening on port %d." % Q.port
 
@@ -47,28 +49,36 @@ gp = open(grav_pos_file, 'r')
 
 count = 0
 for line in gp:
-        gp_list.append(line.split())
-        for i in range(start_timestep, end_timestep):
-                command = "python grav.py" +i+"_density_grid.txt %s %s %s %s" % (gp_list[count][0], gp_list[count][1], gp_list[count][2], gp_list[count][3])
-                print command
-                outfile = sys.argv[3]+"/outputs/"+str(gp_list[count][0]) +"_"+i+"_density_grid.txt.out"
-                print outfile
-                count += 1
-                T = Task(command)
-                T.specify_file(grav_file, grav_file, WORK_QUEUE_INPUT, cache = TRUE)
-                T.specify_file(density_file, density_file, WORK_QUEUE_INPUT, cache = TRUE)
-                T.specify_file(outfile, outfile, WORK_QUEUE_OUTPUT, cache = FALSE)
-                taskid = Q.submit(T)
-print "Done."
+	gp_list.append(line.split())
+	for i in range(start_timestep, end_timestep+1): # +1, because of the way Python runs its ranges
+		density_file = "%d_density_grid.txt" % (i)
+		id = str(gp_list[count][0])
+		outfile = id+"_%d_density_grid.txt.out" % (i)
+		print outfile
+		
+		#command = "python grav_per_point.py " + density_file + " %s %s %s %s" % (gp_list[count][0], gp_list[count][1], gp_list[count][2], gp_list[count][3])
+		command = "python grav_per_point.py " + density_file + " %s %s %s %s > %s" % (gp_list[count][0], gp_list[count][1], gp_list[count][2], gp_list[count][3], outfile)
+		print command
+		T = Task(command)
+		T.specify_file(script_file, script_file, WORK_QUEUE_INPUT, cache = True)
+		T.specify_file("prism.py", "prism.py", WORK_QUEUE_INPUT, cache = True)
+		T.specify_file(grav_pos_file, grav_pos_file, WORK_QUEUE_INPUT, cache = True)
+		T.specify_file(density_file, density_file, WORK_QUEUE_INPUT, cache = True)
+		T.specify_file(outfile, outfile, WORK_QUEUE_OUTPUT, cache = False)
+		taskid = Q.submit(T)
+	count += 1
 gp.close()
-sys.exit(0)
+print "Done."
 
  
 print "Waiting for tasks to complete..."
 while not Q.empty():
-    T = Q.wait(5)
+    T = Q.wait( queue_wait_time ) # wait specifies how long the queue waits for results from workers
+    """
+    if T is None:
+	print "Queue timed out after %d seconds!" % (queue_wait_time)
+    """
     if T:
         print "Task (id# %d) complete: %s (return code %d)" % (T.id, T.command, T.return_status)
-            print T.result
-
+	print "Task result (%s): %s" % (T.result, T.output)
 print "done."
