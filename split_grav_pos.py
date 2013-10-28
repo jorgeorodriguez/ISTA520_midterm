3f243215d15b22af9f0668297c49cb3cbde659b6
Nick Gizzi <ngizzi@email.arizona.edu>
2013-10-27 20:25:18 -0700

    Updated script that will read in grav_pos file and seperate it into sub folders for benchmarking

--- split_grav_pos.py
+++ split_grav_pos.py
@@ -1,18 +1,22 @@
-list = [1,2,3,4,5]
-e = [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150]
+import os
 
-start = 0
+list = [1,2,3,4,5,10,25,50,100,300,500,750,1000,1250,1500,2000,2500]
+e = open(os.path.realpath("grav_pos.txt"))
+
+file_list=e.read().splitlines()
+
+
+
+position = 0
 
 
 for i in range(len(list)):
     amount = list[i]
-    print "[%d] = %d" % (i, amount)
-    print "---"
+    new_file=open("grav_pos_"+str(list[i])+".txt",'w')
 
-    for d in range(start, start + amount):
-        list_amount = e[d]
-        print "[%d] = %d" % (d, list_amount)
-        start = start + 1
-    print "---"
-    print "Start position " + str(start)
-    print "---"
+
+    for d in range(position, position + amount):
+        new_file.write(str(file_list[d])+"\n")
+        position = position + 1
+
+    new_file.close()
