a438b44b485b870c920213e6902439c7a8569821
Nick Gizzi <ngizzi@email.arizona.edu>
2013-10-29 01:09:53 -0700

    newest version of the splitting_densityGrid.py script

--- splitting_densityGrid.py
+++ splitting_densityGrid.py
@@ -10,7 +10,6 @@ file=open(gravPosFile.strip(),'r')
 
 firstLine=file.readline().strip()
 ts=str(0)+firstLine[0]
-print ts
 
 
 new_file=open("density_grid_"+ts,'w')
@@ -21,7 +20,6 @@ new_file.write(firstLine+"\n")
 for line in file:
     temp=line[0:line.find(" ")] #splitting the line into a list, its being seperated by white spaces.
     temp=int(temp)
-    print type(temp)
     if line =="": #checks to see if there is a blank line in the file
         continue
 
@@ -31,7 +29,6 @@ for line in file:
     else:
         new_file.close()
 
-        #if temp=="1" or temp=="2" or temp=="3" or temp=="4" or temp=="5" or temp=="6" or temp=="7" or temp=="8" or temp =="9":  #cant convert input from file to int, dont know why. This can be better coded.
         if temp<10:
 
             ts=str(0)+str(temp)#adds a 0 in front of any number less than 10
@@ -41,7 +38,6 @@ for line in file:
 
         else:
             ts=str(temp)
-        
             new_file=open("density_grid_"+str(ts),'a')
             new_file.write(line)
  
