7f384c592fe2009d34ece8451eb0bb0a18f574f2
Nick Gizzi <ngizzi@email.arizona.edu>
2013-10-29 00:19:12 -0700

    added comments to the split_grav_pos.py script

--- split_grav_pos.py
+++ split_grav_pos.py
@@ -1,3 +1,15 @@
+
+# This script uses the grav_pos.txt file that will be used to slowly scale up to determine
+# how long it takes to run N ammount of Ids.
+# It will iterate through the list and grab i many IDs and
+# will then create a file.
+# Input: no additional input is required.  grav_pos.txt must be in same directory
+# Output: A total of 16 files containing time for each ID
+# 
+# Author: Nick Gizzi
+
+
+
 import os
 
 list = [1,2,3,4,5,10,25,50,100,300,500,750,1000,1250,1500,2000,2500]
@@ -12,6 +24,7 @@ position = 0
 
 for i in range(len(list)):
     amount = list[i]
+    
     new_file=open("grav_pos_"+str(list[i])+".txt",'w')
 
 
