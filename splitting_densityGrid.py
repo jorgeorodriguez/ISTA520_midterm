ee645f56100efb7605bcf6b57d71559f3b84b60c
Nick Gizzi <ngizzi@email.arizona.edu>
2013-10-28 21:02:44 -0700

    updated splitting density grid script with comments.

--- splitting_densityGrid.py
+++ splitting_densityGrid.py
@@ -1,8 +1,9 @@
-#This script will take in one input that is dealing with the grav pos
-#It will then go through the file and make sub files of each ID
-#Each file will hold all the data relevent to that ID point
+#This script will take in one input that is dealing with a grav pos file
+#It will then go through the file and make sub files for each Timestep ID
+#Each file will hold all the data that has that ID point
 #
-#
+#Author: Nick Gizzi
+
 
 gravPosFile= raw_input("Enter in the grav pos file to be sorted: ")
 file=open(gravPosFile.strip(),'r')
@@ -23,7 +24,7 @@ file=open(gravPosFile.strip(),'r')
 
 for line in file:
     print line
-    if line =="":
+    if line =="":#checks to see if there is a blank line in the file
         continue
     elif (lastLine==line):
         new_file.write(line)
@@ -36,7 +37,7 @@ for line in file:
     else:
         new_file.close()
         ts=line[0]
-        new_file=open(str(ts)+"_density_grid",'w')
+        new_file=open(str(ts)+"_density_grid",'w')#checks to see if there is a blank line in the file
         new_file.write(line)
  
 
